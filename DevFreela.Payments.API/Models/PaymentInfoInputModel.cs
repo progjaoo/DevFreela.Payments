6ce01beb87a803d10d85637affc394f204014c89
Joao marcos valente <joaomarcoslars.81@gmail.com>
2023-06-26 21:11:57 -0300

    API - Microsserviço: Implementando o microsserviço de pagamento para o DevFreela Monolito

---
+++ DevFreela.Payments.API/Models/PaymentInfoInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DevFreela.Payments.API.Models
+{
+    public class PaymentInfoInputModel
+    {
+        public int IdProject { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public string Cvv { get; set; }
+
+        public string ExpireAt{ get; set; }
+
+        public string FullName{ get; set;}
+
+        public string Amount { get; set; }  
+    }
+}
---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,12 @@
+﻿using DevFreela.Payments.API.Models;
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            /*ainda nao implementei o gateway, apenas para dizer que o pagamento foi realizado com sucesso!*/
+            return Task.FromResult(true);
+        }
+    }
+}
